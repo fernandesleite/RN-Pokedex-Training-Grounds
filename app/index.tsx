7a70d0ed92f141f3849ebfb663f4eb36837eb128
Bruno Fernandes <brunofdsleite@gmail.com>
2024-11-21 08:03:11 -0300

    add PokedexLayoutWrapper

--- app/index.tsx
+++ app/index.tsx
@@ -1,37 +1,25 @@
-import { Text, View, SafeAreaView, ScrollView, StyleSheet } from "react-native";
+import { Text, View} from "react-native";
+import PokedexLayoutWrapper from "@/components/PokedexLayoutWrapper";
 
-export default function Index() {
+const TopBar = () => {
   return (
-      <SafeAreaView  style={{ flex: 1 }}>
-        <ScrollView contentContainerStyle={styles.container}>
-          <View style={styles.topbar}>
-            <Text>Testing</Text>
-          </View>
-          <View style={styles.content}>
-          <Text>Testing</Text>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-  );
+    <View>
+      <Text>Testing TopBar</Text>
+    </View>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#DC0A2D",
-    padding: 4
-  },
-  topbar: {
-    paddingStart: 16,
-    paddingEnd: 16,
-    paddingBottom: 24,
-    paddingTop: 16,
-  },
-  content: {
-    flex: 1,
-    paddingHorizontal: 12,
-    paddingVertical: 24,
-    borderRadius: 20,
-    backgroundColor: "#fff"
-  }
-})
+const Content = () => {
+  return (
+    <View>
+      <Text>Testing Content</Text>
+    </View>
+  )
+}
+export default function Index() {
+  return (
+    <PokedexLayoutWrapper Topbar={TopBar}>
+        <Content/>
+    </PokedexLayoutWrapper>
+  );
+}
